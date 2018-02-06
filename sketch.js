383fd5e17ac59309696230a2b885136ab51173a6
Melanie Lindow <Lindow@Melanie-Lindows-MacBook-Pro-2.local>
2018-02-05 23:06:15 -0800

    First commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var n = 0;
+var c = 4;
+
+function setup() {
+    createCanvas(400, 400);
+    background(0);
+}
+
+function draw() {
+    var a = n*137.5;
+    var r = c*sqrt(n);
+    var x = r*cos(a) + width/2;
+    var y = r*sin(a) + height/2;
+    fill(255);
+    ellipse(x,y,4,4);
+    n++;
+}
